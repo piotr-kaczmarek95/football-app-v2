3082a0cfd8247fd1e5dfd71a843db1538a26b0f3
Your Name <you@example.com>
2023-01-03 22:23:16 +0100

    code formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
-import { SearchBoxComponent } from './search-box/search-box.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptorService } from './services/app-interceptor.service';
+import { AppComponent } from './app.component'
+import { SearchBoxComponent } from './search-box/search-box.component'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { AuthInterceptorService } from './services/app-interceptor.service'
 
-import { FormsModule } from '@angular/forms';
-import { MainComponent } from './main/main.component';
+import { FormsModule } from '@angular/forms'
+import { MainComponent } from './main/main.component'
 
 import { Routes, RouterModule } from '@angular/router'
-import { MatchDataResolver } from './services/match-data-resolver.service';
-import { AlertComponent } from './alert/alert.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { MatchListComponent } from './match-list/match-list.component';
-import { MatchListItemComponent } from './match-list-item/match-list-item.component';
+import { MatchDataResolver } from './services/match-data-resolver.service'
+import { AlertComponent } from './alert/alert.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { DragDropModule } from '@angular/cdk/drag-drop'
+import { MatchListComponent } from './match-list/match-list.component'
+import { MatchListItemComponent } from './match-list-item/match-list-item.component'
 
-const appRoutes : Routes = [
-  {path: '', pathMatch: 'full', component: MainComponent, resolve: { matches: MatchDataResolver} }
+const appRoutes: Routes = [
+  { path: '', pathMatch: 'full', component: MainComponent, resolve: { matches: MatchDataResolver } }
 ]
 
 @NgModule({
@@ -38,7 +38,7 @@ const appRoutes : Routes = [
     DragDropModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [ {
+  providers: [{
     provide: HTTP_INTERCEPTORS,
     useClass: AuthInterceptorService,
     multi: true
